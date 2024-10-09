ca4369bfc137191ae8562f253866af221f23470a
MRSgkce <mursidegokce985@gmail.com>
2024-10-09 17:26:58 +0300

    toplama ve çıkarma işlemleri ilk temel halde yapıldı.

--- MVVMkullanimi/ViewController.swift
+++ MVVMkullanimi/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var labelSonuc: UILabel!
+    @IBOutlet weak var textfieldSayi1: UITextField!
+    
+    @IBOutlet weak var textfieldSayi2: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        labelSonuc.text = "0"
     }
 
-
+    @IBAction func butonCarpma(_ sender: Any) {
+if let alinansayi1 = textfieldSayi1.text, let alinansayi2 = textfieldSayi2.text {
+    if let sayi1=Int(alinansayi1),let sayi2=Int(alinansayi2){
+        let toplam = sayi1 * sayi2
+        labelSonuc.text = String(toplam)
+    }
+        }
+        let alinansayi2 = textfieldSayi2.text
+        
+    }
+    @IBAction func butonToplama(_ sender: Any) {
+        if let alinansayi1 = textfieldSayi1.text, let alinansayi2 = textfieldSayi2.text {
+            if let sayi1=Int(alinansayi1),let sayi2=Int(alinansayi2){
+                let toplam = sayi1 + sayi2
+                labelSonuc.text = String(toplam)
+            }
+                }
+                let alinansayi2 = textfieldSayi2.text
+        
+    }
+    
 }
 
